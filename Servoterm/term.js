fc821fd72b0f103c67374f553595f9dd759ffaab
Rene Hopf <renehopf@mac.com>
2017-07-18 21:45:19 +0200

    config working

--- Servoterm/term.js
+++ Servoterm/term.js
@@ -21,6 +21,8 @@ var trigger_enabled = false;
 var trigger_edge = true; //true = rising  false = falling
 var trigger_buttonstate = 0; //0 disabled; 1 wait for trigger; 2 trigrd
 var trigger_buttonstate_last = 0;
+var redirect_en = 0;
+var redirect_buf = '';
 
 var uitime = setInterval(refresh_UI, 200);
 
@@ -154,11 +156,15 @@ function receive(info){
 		}else{
 			//TODO: is there a better way?
 			var str = String.fromCharCode.apply(null, [buf[i]]);
-			if(str == '\n'){
-				txt = txt + "<br />";
-			}else{
-				txt = txt + str;
-			}
+         if(redirect_en == 0){
+			   if(str == '\n'){
+				   txt = txt + "<br />";
+			   }else{
+				   txt = txt + str;
+			   }
+         }else{
+            redirect_buf = redirect_buf + str;
+         }
 		}
 	}
 	if(txt.length > 0){//an empty string causes the view to scroll all the time
@@ -309,8 +315,6 @@ function plot(value){
 }
 
 function resize(){
-	console.log("resize");
-
    //console.log(window.devicePixelRatio);
    plotxpos = 0;
 	var canvas = document.getElementById('wavecanvas');
@@ -335,15 +339,12 @@ function resize(){
    var x_res = canvas.width;
    var y_res = canvas.height;
 
-console.log(x_res);
-console.log(y_res);
-
 	var ctx = canvas.getContext('2d');
 	ctx.beginPath();
 	ctx.strokeStyle= "grey";
    ctx.lineWidth = pixel;
 
-	
+	/*
 	//cross
    ctx.moveTo(0,0);
    ctx.lineTo(x_res, y_res);
@@ -362,7 +363,7 @@ console.log(y_res);
 
    ctx.moveTo(0, 0);
    ctx.lineTo(0, y_res);
-	
+	*/
 
 	//centerline
    ctx.moveTo(0, y_res/2);
@@ -445,6 +446,14 @@ function onkeydown(e){
    }
 }
 
+//read configuration
+function filltext(){
+   redirect_en = 0;
+   var configtext = document.getElementById('configtext');
+   configtext.value = redirect_buf;
+   configtext.disabled = false;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   resize();
   window.addEventListener("resize", resize);
@@ -461,32 +470,53 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('layout').addEventListener("dragover", ondragover);
   document.getElementById('enabletrg').addEventListener("click", ontrigger);
   
-  
-  
-  
   // Get the modal
-var modal = document.getElementById('myModal');
+  var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+  // Get the button that opens the modal
+  var btn = document.getElementById("configbutton");
+  var savebutton = document.getElementById("configsavebutton");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+  // When the user clicks on the button, open the modal 
+  btn.onclick = function() {
+     if(!connected){
+        println("not connected");
+        return
+     }
+     redirect_buf = '';
+     redirect_en = 1;
+     chrome.serial.send(connid, convertStringToArrayBuffer('showconf' + '\n'), sendcb);
+     window.setTimeout(filltext, 100);
+     var configtext = document.getElementById('configtext');
+     configtext.value = '';
+     configtext.disabled = true;
+     modal.style.display = "block";
+  }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+  savebutton.onclick = function() {
+     var configtext = document.getElementById('configtext');
+     var lines = configtext.value.split('\n');
+     chrome.serial.send(connid, convertStringToArrayBuffer('deleteconf' + '\n'), sendcb);
+     for(var line = 0; line < lines.length; line++){
+        txqueue.push('appendconf ' + lines[line]);
+        //println(lines[line]);
+        //chrome.serial.send(connid, convertStringToArrayBuffer('appendconf ' + lines[line] + '\n'), sendcb);
+     }
+     modal.style.display = "none";
+  }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+     modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+     if (event.target == modal) {
         modal.style.display = "none";
-    }
+     }
 }
 });
